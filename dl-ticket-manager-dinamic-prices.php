dd70d2e7f23e4972f5e2c46852b1c9135dae864d
Daniel Lúcia <daniellucia84@gmail.com>
2025-08-29 17:03:01 +0200

    Iniciamos extensión

---
+++ dl-ticket-manager-dinamic-prices.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Plugin Name: Dinamic prices for Ticket Manager
+ * Description: Gestión de precios dinámicos para el gestor de tickets.
+ * Version: 0.0.2
+ * Author: Daniel Lúcia
+ * Author URI: http://www.daniellucia.es
+ * textdomain: dl-ticket-manager-dinamic-prices
+ * Requires Plugins: dl-ticket-manager
+ */
+
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/src/Plugin.php';
+
+add_action('plugins_loaded', function () {
+
+    load_plugin_textdomain('dl-ticket-manager-dinamic-prices', false, dirname(plugin_basename(__FILE__)) . '/languages');
+
+    $plugin = new TMDinamicPricesPlugin();
+    $plugin->init();
+});
---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+class TMDinamicPricesPlugin
+{
+
+    private $units;
+
+    public function init(): void
+    {
+       
+    }
+
+}
